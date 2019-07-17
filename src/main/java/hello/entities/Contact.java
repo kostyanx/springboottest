134b31f8b5e4039fb192b6f23cab87686da0a84e
Konstantin Melnikov <melkonnik@gmail.com>
2019-07-17 22:41:47 +0300

    init

---
+++ src/main/java/hello/entities/Contact.java
@@ -0,0 +1,40 @@
+package hello.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long contactId;
+
+    private String firstName;
+
+    private String lastName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName + "]";
+    }
+
+}
