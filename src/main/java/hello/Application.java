a22ca093b34ebc9e32d6a00845c43d9d76476f48
Konstantin Melnikov <melkonnik@gmail.com>
2019-07-18 02:25:45 +0300

    sql exception handling

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,10 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     
     public static void main(String[] args) {
@@ -24,6 +30,15 @@ public class Application {
             System.out.println("Applications:");
             applicationRepository.findAll().forEach(System.out::println);
         };
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 //    not found UPPER CASE strategy
